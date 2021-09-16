934937f27e4c6de4c166d90bd885084f0fd02979
Luis Alejandro Quiroz <luis.alejandro.quiroz@gmail.com>
2021-09-15 21:26:02 -0500

    Initial commit LAQ

---
+++ config/initializers/spotify_setup.rb
@@ -0,0 +1,1 @@
+RSpotify::authenticate('695f9df1d08744b48b9a1ed570ca1a9f', '621d95a82f274535b833505fa6f3ce5b')
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    namespace :api do
+      namespace :v1 do
+        resources :tracks do
+          collection do
+            get :top_100
+            get :random
+            get :search
+          end
+        end
+      end
+    end
+  end
 end
